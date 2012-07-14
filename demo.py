b6ef41739b38a72979752f30f3d8f5406b836a99
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-07-14 03:56:05 +0200

    Added demo assertions that show that the line numbers are really preserved in stacktraces etc

--- demo.py
+++ demo.py
@@ -3,10 +3,17 @@ import matchsyntax
 matchsyntax.register_match_importer()
 
 import dis
+import traceback
 
 import example
 
-example.test(('test', 1))
+try:
+    example.test(('test', 1))
+except example.TestException:
+    e = traceback.format_exc()
+    print "GOT TRACEBACK", e
+    assert 'File "example.py", line 22, in test' in e
+    assert 'raise TestException("let\'s look at the stack trace")' in e
 
 print "---------"
 dis.dis(example.test)
--- example.py
+++ example.py
@@ -1,8 +1,22 @@
 #- LANGUAGE match-statement -#
 
+import inspect
+
+def lineno():
+    """Returns the current line number in our program."""
+    return inspect.currentframe().f_back.f_lineno
+
+
+class TestException(Exception):
+    pass
+
+
 def test(msg):
     match msg:
         case ('test', a):
             print "TEST", msg
+            print "LINE NR SHOULD BE 18", lineno()
+            assert lineno() == 19
         case ('other', a):
             print "OTHER", msg
+            raise TestException("let's look at the stack trace")
