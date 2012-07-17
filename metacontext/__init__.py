862a0592df05f69dae7c8770a259d93de6bccea5
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-07-17 18:00:21 +0200

    Fixed import hook for importing stuff from other packages and paths

--- metacontext/__init__.py
+++ metacontext/__init__.py
@@ -1,5 +1,6 @@
 import ast
 import inspect
+import importlib
 import new
 import os
 import sys
@@ -10,21 +11,48 @@ class TranslatorImportHook(object):
     def __init__(self, tag=None):
         self.tag = tag
 
-    def src_name(self, fullname):
-        return '%s.py' % fullname
+    def src_name(self, p, fullname):
+        module_name = fullname.rsplit('.', 1)[-1]
+
+        return os.path.join(p, '%s.py' % module_name)
 
     def find_module(self, fullname, path=None):
-        if os.path.exists(self.src_name(fullname)):
+        if '.' in fullname:
+            package_name, module_name = fullname.rsplit('.', 1)
+        else:
+            package_name, module_name = ('', fullname)
+
+        package_path = ['']
+        if package_name:
+            package = importlib.import_module(package_name)
+            package_path = package.__path__
+
+        for p in package_path:
+            loader = self.find_module_in_path(fullname, p)
+            if loader:
+                return loader
+
+    def find_module_in_path(self, fullname, p):
+        if os.path.exists(self.src_name(p, fullname)):
             # if the import hook was registered with a restriction tag,
             # we have to check if it's present in the file
             if self.tag:
-                with open(self.src_name(fullname)) as f:
+                with open(self.src_name(p, fullname)) as f:
                     if f.readline().startswith('#- LANGUAGE %s -#' % self.tag):
-                        return self
+                        return TranslatorLoader(p)
             # otherwise we just translate every single import
             else:
-                return self
+                return TranslatorLoader(p)
         return None
+
+class TranslatorLoader(object):
+    def __init__(self, path):
+        self.path = path
+
+    def src_name(self, fullname):
+        module_name = fullname.rsplit('.', 1)[-1]
+
+        return os.path.join(self.path, '%s.py' % module_name)
 
     def load_module(self, fullname):
         print "Compiling module with match statement:", fullname
@@ -50,7 +78,7 @@ class TranslatorImportHook(object):
 
         for node in ast.iter_child_nodes(tree):
             if isinstance(node, ast.ImportFrom):
-                mod = __import__(node.module)
+                mod = importlib.import_module(node.module)
                 for alias in node.names:
                     name = alias.name
                     as_name = alias.asname or name
