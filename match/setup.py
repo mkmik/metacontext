5312e99e9ce96bda6ac022814740410097796def
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-07-17 16:55:20 +0200

    Moved match demo to subproject

---
+++ match/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from version import get_git_version
+
+
+setup(
+    name = "metacontext.match",
+    version = get_git_version(),
+    description = """Example match/case statement for metacontext""",
+    author = "Marko Mikulicic",
+    author_email = "mmikulicic@gmail.com",
+    packages = find_packages(),
+    entry_points = {'console_scripts': ['demo = match.demo:run']},
+    install_requires = ['metacontext']
+)
